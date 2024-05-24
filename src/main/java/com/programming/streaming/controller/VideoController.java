56e8ae17e599acef368b80615cf01fd179218785
Trinh-Tan-Dat <21520714@gm.uit.edu.vn>
2024-05-24 20:17:52 +0700

    update

--- src/main/java/com/programming/streaming/controller/VideoController.java
+++ src/main/java/com/programming/streaming/controller/VideoController.java
@@ -19,80 +19,70 @@ import java.sql.Timestamp;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 @RestController
-@CrossOrigin("*")
 @RequestMapping("/video")
 @AllArgsConstructor
 public class VideoController {
 
-    @GetMapping("")
+    @GetMapping("/")
     public String getVideo() {
         return "Video service";
     }
 
 
+    @Autowired
+    private VideoService videoService;
 
-    // @Autowired
-    // private VideoService videoService;
+    @PostMapping("/upload")
+    public ResponseEntity<?> upload(@RequestParam("file") MultipartFile file,
+            @RequestParam("userID") String userID,
+            @RequestParam("thumbnail") MultipartFile thumbnailFile) throws IOException {
+        byte[] thumbnail = thumbnailFile.getBytes();
+        Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+        return new ResponseEntity<>(videoService.addVideo(file, userID, thumbnail, timestamp), HttpStatus.OK);
+    }
 
-    // @CrossOrigin(origins = "*")
-    // @PostMapping("/upload")
-    // public ResponseEntity<?> upload(@RequestParam("file") MultipartFile file,
-    //         @RequestParam("userID") String userID,
-    //         @RequestParam("thumbnail") MultipartFile thumbnailFile) throws IOException {
-    //     byte[] thumbnail = thumbnailFile.getBytes();
-    //     Timestamp timestamp = new Timestamp(System.currentTimeMillis());
-    //     return new ResponseEntity<>(videoService.addVideo(file, userID, thumbnail, timestamp), HttpStatus.OK);
-    // }
+    @GetMapping("/get/{id}")
+    public ResponseEntity<ByteArrayResource> download(@PathVariable String id) throws IOException {
+        Video loadFile = videoService.getVideo(id);
 
-    // @CrossOrigin(origins = "*")
-    // @GetMapping("/get/{id}")
-    // public ResponseEntity<ByteArrayResource> download(@PathVariable String id) throws IOException {
-    //     Video loadFile = videoService.getVideo(id);
+        return ResponseEntity.ok()
+                .contentType(MediaType.parseMediaType(loadFile.getFileType()))
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + loadFile.getFilename() + "\"")
+                .body(new ByteArrayResource(loadFile.getFile()));
+    }
 
-    //     return ResponseEntity.ok()
-    //             .contentType(MediaType.parseMediaType(loadFile.getFileType()))
-    //             .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + loadFile.getFilename() + "\"")
-    //             .body(new ByteArrayResource(loadFile.getFile()));
-    // }
+    @GetMapping("/getAllIds")
+    public ResponseEntity<?> getAllID() {
+        return new ResponseEntity<>(videoService.getAllVideoIDs(), HttpStatus.OK);
+    }
 
-    // @CrossOrigin(origins = "*")
-    // @GetMapping("/getAllIds")
-    // public ResponseEntity<?> getAllID() {
-    //     return new ResponseEntity<>(videoService.getAllVideoIDs(), HttpStatus.OK);
-    // }
+    @GetMapping("/listIdThumbnail")
+    public ResponseEntity<?> listIdThumbnail() {
+        return new ResponseEntity<>(videoService.listIdThumbnail(), HttpStatus.OK);
+    }
 
-    // @CrossOrigin(origins = "*")
-    // @GetMapping("/listIdThumbnail")
-    // public ResponseEntity<?> listIdThumbnail() {
-    //     return new ResponseEntity<>(videoService.listIdThumbnail(), HttpStatus.OK);
-    // }
+    @GetMapping("/getVideoIdFromThumbnailId/{id}")
+    public ResponseEntity<?> getVideoIdFromThumbnailId(@PathVariable String id) {
+        return new ResponseEntity<>(videoService.getVideoIdFromThumbnailId(id), HttpStatus.OK);
+    }
 
-    // @CrossOrigin(origins = "*")
-    // @GetMapping("/getVideoIdFromThumbnailId/{id}")
-    // public ResponseEntity<?> getVideoIdFromThumbnailId(@PathVariable String id) {
-    //     return new ResponseEntity<>(videoService.getVideoIdFromThumbnailId(id), HttpStatus.OK);
-    // }
+    @PutMapping("/updateViews/{id}")
+    public ResponseEntity<?> updateViews(@PathVariable String id) {
+        videoService.updateViews(id);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
 
-    // @CrossOrigin(origins = "*")
-    // @PutMapping("/updateViews/{id}")
-    // public ResponseEntity<?> updateViews(@PathVariable String id) {
-    //     videoService.updateViews(id);
-    //     return new ResponseEntity<>(HttpStatus.OK);
-    // }
+    @PutMapping("/updateLikes/{id}")
+    public ResponseEntity<?> updateLikes(@PathVariable String id) {
+        videoService.updateLikes(id);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
 
-    // @CrossOrigin(origins = "*")
-    // @PutMapping("/updateLikes/{id}")
-    // public ResponseEntity<?> updateLikes(@PathVariable String id) {
-    //     videoService.updateLikes(id);
-    //     return new ResponseEntity<>(HttpStatus.OK);
-    // }
-
-    // @CrossOrigin(origins = "*")
-    // @PutMapping("/updateDislikes/{id}")
-    // public ResponseEntity<?> updateDislikes(@PathVariable String id) {
-    //     videoService.updateDislikes(id);
-    //     return new ResponseEntity<>(HttpStatus.OK);
-    // }
+    @PutMapping("/updateDislikes/{id}")
+    public ResponseEntity<?> updateDislikes(@PathVariable String id) {
+        videoService.updateDislikes(id);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
 
     
 }
